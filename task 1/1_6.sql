97c8c31c3f2b948904dcd8e5187c0cfddec3e7c2
anastasiakutnenko <32202148+anastasiakutnenko@users.noreply.github.com>
2019-06-11 11:44:00 +0300

    Update 1_6.sql

--- task 1/1_6.sql
+++ task 1/1_6.sql
@@ -1,7 +1,2 @@
-select posts.*, group_concat(tags.title) as tags from mydb.posts 
-left join mydb.posts_has_tags
-on posts.id = posts_has_tags.posts_id
-left join mydb.tags
-on tags.tag_id = posts_has_tags.tags_tag_id
-where posts.title like 'S%'
-group by posts.title, posts.id; 
+select posts.* from mydb.posts 
+where posts.title like 'S%';
